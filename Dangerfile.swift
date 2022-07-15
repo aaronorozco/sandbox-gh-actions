4703337c76698b98ebfcbc1089a75a6b61599d5c
aaron <aarono562@gmail.com>
2022-07-15 13:22:34 -0700

    danger

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,5 +1,10 @@
-import Danger 
+import Danger
+import DangerXCodeSummary
+
 let danger = Danger()
 
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
+
+let summary = XCodeSummary(filePath: "result/json")
+summary.report()
